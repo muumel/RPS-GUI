643950d0bc6917d3664397e66b4db758dbb0ddf0
Sukanya Chinwicha <mennoi_2544@hotmail.com>
2021-03-26 11:55:14 +0700

    Login-page

--- Desktop/reactProject/shopping-web-project/src/pages/Login/Login.js
+++ Desktop/reactProject/shopping-web-project/src/pages/Login/Login.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import './Login.css';
+import React from "react";
+import "./Login.css";
 
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <section id="boxCardLogin">
+        <article>
+          <h2>Welcome Member</h2>
+          <form>
+            <div className="input-container focused input-filled">
+              <input id="email" className="email" type="email" name="email" />
+              <label for="email">Email Address</label>
+            </div>
+            <div className="input-container focused">
+              <input
+                id="password"
+                className="password"
+                type="password"
+                name="password"
+              />
+              <label for="password">Password</label>
+            </div>
+            <a href="#" className="reset-pwd">
+              Forget password?
+            </a>
+
+            <button href="#" className="btn" type="submit">
+              Submit
+            </button>
+          </form>
+          <p>
+            Not a Member?{" "}
+            <a href="#" className="register">
+              Register Now
+            </a>
+          </p>
+        </article>
+      </section>
+    </div>
+  );
+};
 
 export default Login;
